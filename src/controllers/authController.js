ae3c5172ff64c2ed624635e1c9f90e52a05c1da0
Gurpreet <gurpreet.singh@attplgroup.com>
2024-10-08 05:16:24 +0530

    update comment

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,14 @@
 const passport = require('passport');
 
-// @desc    Initiates Google OAuth2 login
-// @route   GET /auth/google
+
 exports.googleLogin = passport.authenticate('google', {
   scope: ['profile', 'email'],
 });
 
-// @desc    Callback after Google OAuth2 login success
-// @route   GET /auth/google/callback
+
 exports.googleCallback = (req, res, next) => {
   passport.authenticate('google', { failureRedirect: '/' }, async (err, user) => {
-    
+
     if (err) {
       return next(err);
     }
@@ -18,29 +16,22 @@ exports.googleCallback = (req, res, next) => {
       return res.redirect('/');
     }
 
-    // Manually log in the user (since we're using a custom callback)
     req.login(user, (loginErr) => {
       if (loginErr) {
         return next(loginErr);
       }
 
-      // Optionally, perform additional logic here (e.g., sending data to frontend)
-
-      // Redirect to the frontend dashboard after successful login
       res.redirect('https://foyr-backend.vercel.app/dashboard');
     });
   })(req, res, next);
 };
 
-// @desc    Logout the user 
-// @route   GET /auth/logout
+
 exports.logout = (req, res) => {
   req.logout();
   res.redirect('/');
 };
 
-// @desc    Get the authenticated user's session
-// @route   GET /auth/session
 exports.getSession = (req, res) => {
   if (req.isAuthenticated()) {
     res.json({
